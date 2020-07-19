adbfc1da6ddd6677154a9ef636dc231ddcfb4e52
LucasStoccoSoldera <67830589+LucasStoccoSoldera@users.noreply.github.com>
2020-07-19 18:37:35 -0300

    Add files via upload

---
+++ CALCULADORA_0_1_2/CALCULADORA2.0/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CALCULADORA2._0
+{
+   
+    public partial class Form1 : Form
+    {
+        string op, n1, n2;
+        Conta ChamadodeClasse = new Conta();
+        Int32 contVirgula = 0;
+        public void ajusteVirgula()
+        {
+            if (contVirgula == 0)
+            {
+                escrevedor(",");
+                contVirgula = 1;
+            }
+        }
+        public Form1()
+        {
+           
+            InitializeComponent();
+        }
+
+        public void escrevedor(String valor)
+        {
+                textBox1.Text = textBox1.Text + valor;
+            
+        }   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            escrevedor("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            escrevedor("2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            escrevedor("3");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            escrevedor("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            escrevedor("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            escrevedor("6");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            escrevedor("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            escrevedor("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            escrevedor("9");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            escrevedor("0");
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            //Conta ChamadodeClasse = new Conta();
+            //ChamadodeClasse.fazedordeconta("+", textBox1.Text);
+            op = "+";
+            n1 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            //Conta ChamadodeClasse = new Conta();
+            //ChamadodeClasse.fazedordeconta("-", textBox1.Text);
+            op = "-";
+            n1 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            //Conta ChamadodeClasse = new Conta();
+            //ChamadodeClasse.fazedordeconta("*", textBox1.Text);
+            op = "*";
+            n1 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            //Conta ChamadodeClasse = new Conta();
+            //ChamadodeClasse.fazedordeconta("/", textBox1.Text);
+            op = "/";
+            n1 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Conta ChamadodeClasse = new Conta();
+            ChamadodeClasse.n2 = textBox1.Text;
+
+            if (ChamadodeClasse.n2 == "0")
+            {
+                MessageBox.Show(ChamadodeClasse.fazedordeconta(op, n1), "Presta Atenção!");
+            }
+            else
+            {
+                textBox1.Text = ChamadodeClasse.fazedordeconta(op, n1);
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText(@"Saves.txt", textBox1.Text);
+            label1.Text = "Ta mec, o conteúdo digitado foi salvo.";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            try
+            {
+
+                if (System.IO.File.Exists(@"Saves.txt") == true) 
+                if (System.IO.File.ReadAllText(@"Saves.txt") != "") 
+                        textBox1.Text = System.IO.File.ReadAllText(@"Saves.txt");
+                label1.Text = "Ta na mão.";
+            }
+            catch
+            {
+                MessageBox.Show("Houve um erro inesperado! Não foi possível efetuar a leitura.");
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            label1.Text = "A sua disposição chefia!";
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                var result = MessageBox.Show(this, "Você tem certeza que deseja fechar essa maravilhosa calculadora?", "Confirmação de Fechamento!", MessageBoxButtons.YesNo);
+                if (result != DialogResult.Yes)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            System.IO.File.WriteAllText(@"Saves.txt", "");
+            textBox1.Text = "";
+            label1.Text = "Deletado com sucesso.";            
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            ajusteVirgula();
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Sobre about = new Sobre();
+            about.Show();
+        }
+
+        private void btPorcentagem_Click(object sender, EventArgs e)
+        {
+            op = "%";
+            n1 = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            op = "";
+            n1 = "";
+            n2 = "";
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ CALCULADORA_0_1_2/CALCULADORA2.0/Sobre.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace CALCULADORA2._0
+{
+    partial class Sobre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Sobre));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.Font = new System.Drawing.Font("Noto Sans", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(76, 160);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(601, 113);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Sobre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.Color.LimeGreen;
+            this.Name = "Sobre";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Informações Importantes Sobre Este Protótipo de Calculadora";
+            this.Load += new System.EventHandler(this.Sobre_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
